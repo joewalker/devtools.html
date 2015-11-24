2382bc1ec67ea8741202dc591d6dafbe07763bfe
J. Ryan Stinnett <jryans@gmail.com>
2015-11-24 13:12:01 -0600

    Basic WS <-> TCP proxy

---
+++ tools/ws-tcp-proxy/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const ws = require("ws");
+const net = require("net");
+
+const WEB_SOCKET_PORT = 9000;
+const TCP_PORT = 6080;
+
+let wsServer = new ws.Server({ port: WEB_SOCKET_PORT });
+
+console.log("Listening for WS on *:" + WEB_SOCKET_PORT);
+console.log("Will proxy to TCP on *:" + TCP_PORT + " on first WS connection");
+
+wsServer.on("connection", wsConnection => {
+  let tcpClient = net.connect({ port: TCP_PORT });
+  tcpClient.setEncoding("utf8");
+
+  tcpClient.on("connect", () => {
+    console.log("TCP connection succeeded");
+  });
+
+  tcpClient.on("error", e => {
+    wsConnection.close();
+    console.log("TCP connection failed: " + e);
+  });
+
+  tcpClient.on("data", data => {
+    console.log("TCP -> WS: " + data);
+    wsConnection.send(data);
+  });
+
+  wsConnection.on("message", msg => {
+    console.log("WS -> TCP: " + msg);
+    tcpClient.write(msg);
+  });
+});
