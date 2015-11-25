27774e4b8ceba39b7cb706ecc6bef964658c8e8d
J. Ryan Stinnett <jryans@gmail.com>
2015-11-25 00:47:04 -0600

    Add inspector to connection test page

--- tools/connect/index.js
+++ tools/connect/index.js
@@ -5,6 +5,7 @@ let { DebuggerClient } = require("devtools/shared/client/main");
 let { DebuggerTransport } = require("devtools/shared/transport/transport");
 let { Task } = require("devtools/sham/task");
 let { TargetFactory } = require("devtools/client/framework/target");
+let { InspectorFront } = require("devtools/server/actors/inspector");
 
 exports.start = Task.async(function*() {
   let socket = new WebSocket("ws://localhost:9000");
@@ -23,8 +24,15 @@ exports.start = Task.async(function*() {
     chrome: false,
   };
   let target = yield TargetFactory.forRemoteTab(options);
-  output.textContent += target;
+  output.textContent += target + "\n\n";
 
   /*let hostType = Toolbox.HostType.WINDOW;
   gDevTools.showToolbox(target, tool, hostType)*/
+
+  let inspector = InspectorFront(target.client, target.form);
+  output.textContent += inspector + "\n\n";
+
+  let walker = yield inspector.getWalker();
+
+  output.textContent += walker + "\n\n";
 });
