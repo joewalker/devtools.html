6d26df6ff14813cc7c15f324e8e381609b888089
Tom Tromey <tom@tromey.com>
2015-11-25 08:59:50 -0700

    add parse-css and inIDOMUtils.getCSSLexer

--- sham/chrome.js
+++ sham/chrome.js
@@ -2,19 +2,30 @@
  * A sham for https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs/chrome
  */
 
+var { CSSLexer } = require("devtools/sham/parse-css");
+
+var ourServices = {
+  inIDOMUtils: {
+    getCSSLexer: function(text) {
+      return new CSSLexer(text);
+    },
+  },
+};
+
 module.exports = {
   Cc: name => {
     console.log('Sham for', name);
     return {
-      getService: () => {}
+      getService: (name) => ourServices[name]
     };
   },
   Ci: {
     nsIFocusManager: {
       MOVEFOCUS_BACKWARD: 2,
       MOVEFOCUS_FORWARD: 1,
+    },
 
-    }
+    inIDOMUtils: "inIDOMUtils",
   },
   Cu: {
     reportError: (msg) => console.error(msg),
