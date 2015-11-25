17c02d0c6ad832e32086970a7b17805b56494b5f
J. Ryan Stinnett <jryans@gmail.com>
2015-11-24 22:29:18 -0600

    Rewrite connect tool with Task / promises

--- tools/connect/index.js
+++ tools/connect/index.js
@@ -3,21 +3,14 @@
 
 let { DebuggerClient } = require("devtools/shared/client/main");
 let { DebuggerTransport } = require("devtools/shared/transport/transport");
+let Task = require("devtools/sham/task");
 
-let gClient;
-
-exports.start = () => {
+exports.start = Task.async(function*() {
   let socket = new WebSocket("ws://localhost:9000");
   let transport = new DebuggerTransport(socket);
-  gClient = new DebuggerClient(transport);
-  gClient.connect(onConnect);
-};
-
-function onConnect() {
-  gClient.listTabs(onTabs);
-}
-
-function onTabs(response) {
+  let client = new DebuggerClient(transport);
+  yield client.connect();
+  let tabs = yield client.listTabs();
   let output = document.getElementById("output");
-  output.textContent = JSON.stringify(response, null, 2);
-}
+  output.textContent = JSON.stringify(tabs, null, 2);
+});
