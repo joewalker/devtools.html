f25effd71572730527d17eab56ef89270921a607
Joe Walker <jwalker@mozilla.com>
2015-11-26 12:15:40 +0000

    An attempt at fixing the build for IE/Chrome

--- client/inspector/webpack.config.js
+++ client/inspector/webpack.config.js
@@ -2,7 +2,7 @@ var common = require("../../build/common.webpack.config");
 
 module.exports = Object.assign({}, common, {
   context: __dirname,
-  entry: "./inspector-panel.js",
+  entry: [ "babel-polyfill", "./inspector-panel.js" ],
   output: {
     path: __dirname,
     filename: "build.js",
