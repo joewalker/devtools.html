cdd029a1c41b82255e9e7ce4eee937f734edc64a
James Long <longster@gmail.com>
2015-12-01 14:57:26 -0500

    turn off sourcemapping and auto-blackboxing in debugger

--- client/preferences/devtools.js
+++ client/preferences/devtools.js
@@ -80,10 +80,10 @@ pref("devtools.debugger.remote-host", "localhost");
 pref("devtools.debugger.remote-timeout", 20000);
 pref("devtools.debugger.pause-on-exceptions", false);
 pref("devtools.debugger.ignore-caught-exceptions", true);
-pref("devtools.debugger.source-maps-enabled", true);
+pref("devtools.debugger.source-maps-enabled", false);
 pref("devtools.debugger.pretty-print-enabled", true);
 pref("devtools.debugger.auto-pretty-print", false);
-pref("devtools.debugger.auto-black-box", true);
+pref("devtools.debugger.auto-black-box", false);
 pref("devtools.debugger.workers", false);
 pref("devtools.debugger.promise", false);
 
